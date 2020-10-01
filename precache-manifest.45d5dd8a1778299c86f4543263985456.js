4a750e9c5691d862509fb4648e059230910fa8a4
Muhammad Al Farizzi <muhammadalfarizzi563@gmail.com>
2020-10-01 11:53:12 +0700

    Updates

---
+++ precache-manifest.45d5dd8a1778299c86f4543263985456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d4289d9e717f3e0ad0dd59db644449",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "a8fb4d2519d50d6f7f62",
+    "url": "/covid-tracker/static/js/2.dfae27ee.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/covid-tracker/static/js/2.dfae27ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7356c6884d33fbf987",
+    "url": "/covid-tracker/static/js/main.2fee4c14.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
